ec3665007d26faae9a00f977eb713491e8c9e479
Iñaki Liendo <sebastian@liendo.net>
2024-12-31 17:41:58 -0600

    migrated gromov -> cartan

--- static/scripts/upload.js
+++ static/scripts/upload.js
@@ -3,7 +3,7 @@ const lambdaUrl = "https://p32k4p66fx7b24cgnjt5n5kszq0cwiwl.lambda-url.us-east-1
 document.getElementById("upload-exam").addEventListener("submit", (e) => {
 	e.preventDefault();
 	divMsg = document.getElementById("upload-message");
-	divMsg.innerHTML = null;
+	divMsg.innerHTML = "subiendo examen...";
 
 	const formData = new FormData(e.target);
 
